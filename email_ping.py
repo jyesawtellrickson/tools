1fba0abbc8875a9de0c600341f4ae29e82ea87b7
Jye <jyesawtellrickson@gmail.com>
2017-06-22 18:46:09 +0800

    Update email_ping.py

--- email_ping.py
+++ email_ping.py
@@ -10,23 +10,13 @@ import dns.resolver
 import socket
 import smtplib
 
-customers_df = pd.read_csv("linkedin_profiles.csv", encoding='utf-8')
-try:
-    tmp = customers_df['verified'][0]
-except:
-    customers_df['verified'] = None
+email_list = pd.read_csv("linkedin_profiles.csv", encoding='utf-8')
 
-try:
-    tmp = customers_df['final_email'][0]
-except:
-    customers_df['final_email'] = None
+check_email_list(email_list)
 
-for i in range(0, 1): #customers_df.shape[0]):
-    # check name and domain are there
-    if customers_df['name'] is None or customers_df['domain'] is None:
-        next(i)
+def ping_email(domain, emails):
     # connect to server
-    records = dns.resolver.query(customers_df['domain'][i], 'MX')
+    records = dns.resolver.query(domain, 'MX')
     mxRecord = records[0].exchange
     mxRecord = str(mxRecord)
 
@@ -36,18 +26,13 @@ for i in range(0, 1): #customers_df.shape[0]):
     # SMTP lib setup (use debug level for full output)
     server = smtplib.SMTP()
     server.set_debuglevel(0)
-
     # SMTP Conversation
     server.connect(mxRecord)
     server.helo(host)
     server.mail('me@domain.com')
 
-    # get emails
-    if customers_df['email'][i].find(";") != -1:
-        emails = customers_df['email'][i].split(";")[:-1]
-    else:
-        emails = customers_df['email'][i]
     verified = False
+    addressToVerify = ''
     while verified == False and len(emails) > 0:
         addressToVerify = emails.pop()
         print(addressToVerify)
@@ -61,10 +46,39 @@ for i in range(0, 1): #customers_df.shape[0]):
             print('Success')
             verified = True
     server.quit()
-    if verified == True:
-        customers_df['verified'][i] = "y"
-        customers_df['final_email'][i] = addressToVerify
-    else:
-        customers_df['verified'][i] = "n"
+    return (addressToVerify, verified)
 
-customers_df.to_csv('linkedin_profiles_checked.csv')
+
+def check_email_list(email_list):
+    customers_df = email_list
+    # create new columns if non-existant
+    try:
+        tmp = customers_df['verified'][0]
+    except:
+        customers_df['verified'] = None
+    try:
+        tmp = customers_df['final_email'][0]
+    except:
+        customers_df['final_email'] = None
+
+    for i in range(0, 1): #customers_df.shape[0]):
+        # check name and domain are there
+        if customers_df['name'] is None or customers_df['domain'] is None:
+            next(i)
+
+        # get emails
+        if customers_df['email'][i].find(";") != -1:
+            emails = customers_df['email'][i].split(";")[:-1]
+        else:
+            emails = customers_df['email'][i]
+        ping_results = ping_email(customers_df['domain'][i], emails)
+        addressToVerify = ping_results[0]
+        verified = ping_results[1]
+
+        if verified == True:
+            customers_df['verified'][i] = "y"
+            customers_df['final_email'][i] = addressToVerify
+        else:
+            customers_df['verified'][i] = "n"
+
+    customers_df.to_csv('linkedin_profiles_checked.csv')
